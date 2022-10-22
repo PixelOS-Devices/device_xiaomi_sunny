ad6c6d386a75c6d55b0f07bfa3605d6a5bcfcd87
Cyber Knight <cyberknight755@gmail.com>
2022-10-22 17:24:17 +0800

    mojito: Build GrapheneCamera
    - I prefer GrapheneCam anyways.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -152,9 +152,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-# GCamGo
+# GrapheneCam
 PRODUCT_PACKAGES += \
-    GCamGOPrebuilt-V2
+    GrapheneCamera
 
 # Charger
 PRODUCT_PACKAGES += \
