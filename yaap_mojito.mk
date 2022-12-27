48ed4a861420885815fa7b2b21422dbbb963a692
Arian <arian.kulmer@web.de>
2022-12-27 13:14:51 +0000

    mojito: Rework init
    * based on https://github.com/xiaomi-sm6150/android_device_xiaomi_davinci/tree/lineage-20/init
    
    Co-authored-by: basamaryan <basam.aryan@gmail.com>
    Change-Id: Ib229b1b0c27a61547b51b2be85a39b5b4eec9680

--- yaap_mojito.mk
+++ yaap_mojito.mk
@@ -23,10 +23,3 @@ PRODUCT_MODEL := Redmi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Build
-BUILD_FINGERPRINT := "Redmi/mojito/mojito:12/RKQ1.210614.002/V13.0.11.0.SKGMIXM:user/release-keys"
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mojito-user 12 RKQ1.210614.002 V13.0.11.0.SKGMIXM release-keys"
