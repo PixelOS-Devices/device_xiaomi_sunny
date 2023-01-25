c171717d13260d6c49d3fdcd7b2fc41134899a2c
Cyber Knight <cyberknight755@gmail.com>
2023-01-25 18:33:05 +0800

    mojito: Drop legacy powerhal double_tap implementation
    - We are moving to a modernized implementation in the upcoming commit.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,7 +160,6 @@ BOARD_USES_QCOM_HARDWARE := true
 
 # Power
 TARGET_POWERHAL_BOOST_EXT := $(DEVICE_PATH)/power/boost-ext.cpp
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab_AB.qcom
