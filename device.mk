07b5afa8ecc634fcdc23aa2af11d6f2e467c4632
blurzyy <selster02@gmail.com>
2023-02-02 10:59:23 +0800

    mojito: Import regulatory info rro
    Change-Id: Ie4872764bb3111c6a6e809e9b61186bc9bcaec80
    Signed-off-by: blurzyy <selster02@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -388,7 +388,9 @@ PRODUCT_PACKAGES += \
     SettingsMojito \
     SystemUIMojito \
     TelephonyMojito \
-    WifiMojito
+    WifiMojito \
+    SettingsOverlayM2101K7AG \
+    SettingsOverlayM2101K7AI
 
 # Perf
 PRODUCT_PACKAGES += \
