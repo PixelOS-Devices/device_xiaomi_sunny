1c4536647686147759ba07b1c4ad50bb10f0cd90
Cyber Knight <cyberknight755@gmail.com>
2023-11-13 17:24:10 +0800

    sunny: Sync perf configs with qcom/common
    - Aswell as use perfboostsconfig.xml from qcom/common.
    
    Change-Id: I307fb1808352d29e3207762518f7cc2f99ad1dd8
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -386,7 +386,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
     $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
 # Platform
